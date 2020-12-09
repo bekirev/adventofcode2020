551de068fd82635e8548e36f12cf66ca13491c32
artechry <abekirev@gmail.com>
2020-12-09 11:23:42 +0300

    day 9 part 2

--- src/main/kotlin/com/abekirev/adventofcode2020/day09/EncodingError.kt
+++ src/main/kotlin/com/abekirev/adventofcode2020/day09/EncodingError.kt
@@ -6,8 +6,10 @@ import java.nio.file.Path
 import java.util.LinkedList
 import java.util.Queue
 
+
 fun main() {
     partOne()
+    partTwo()
 }
 
 private const val PREAMBLE_SIZE = 26
@@ -20,10 +22,36 @@ private fun partOne() =
                 .runningChunked(PREAMBLE_SIZE)
                 .map { it.last() to it.asSequence().take(PREAMBLE_SIZE - 1).findTupleWithSum(it.last(), 2) }
                 .filter { it.second == null }
-                .firstOrNull()
-                ?.first
+                .first()
+                .first
         }
     )
+
+private fun partTwo() =
+    println(
+        Path.of("input", "day09", "input.txt").useLinesFromResource { lines ->
+            lines.map(String::toLong).toList().let { numbers ->
+                numbers.take(numbers.indexOf(1124361034) - 1)
+            }
+                .findSubsequenceWithSpecificSumOfElements(1124361034L)
+                ?.let { it.minOrNull()!! to it.maxOrNull()!! }
+                ?.let { it.first + it.second }
+        }
+    )
+
+private fun List<Long>.findSubsequenceWithSpecificSumOfElements(value: Long): List<Long>? {
+    for (start in 0 until size) {
+        for (end in start + 1..size) {
+            val subList = subList(start, end)
+            val sum = subList.sum()
+            when {
+                sum > value -> break
+                sum == value -> return subList
+            }
+        }
+    }
+    return null
+}
 
 private fun <T> Sequence<T>.runningChunked(chunkSize: Int): Sequence<List<T>> = sequence {
     val queue: Queue<T> = LinkedList()
