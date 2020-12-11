c93b8ab07456a2fe0c39442242f5920e00cc8fec
artechry <abekirev@gmail.com>
2020-12-11 13:11:15 +0300

    day 11 part 2

--- src/main/kotlin/com/abekirev/adventofcode2020/day11/SeatingSystem.kt
+++ src/main/kotlin/com/abekirev/adventofcode2020/day11/SeatingSystem.kt
@@ -1,8 +1,5 @@
 package com.abekirev.adventofcode2020.day11
 
-import com.abekirev.adventofcode2020.day11.Cell.FLOOR
-import com.abekirev.adventofcode2020.day11.Cell.OCCUPIED_SEAT
-import com.abekirev.adventofcode2020.day11.Cell.EMPTY_SEAT
 import com.abekirev.adventofcode2020.util.grid.Grid
 import com.abekirev.adventofcode2020.util.grid.MutableMapGrid
 import com.abekirev.adventofcode2020.util.grid.Position
@@ -14,36 +11,52 @@ import java.nio.file.Path
 
 fun main() {
     partOne()
+    partTwo()
 }
 
 private fun partOne() =
     println(
-        Path.of("input", "day11", "input.txt").useLinesFromResource { lines ->
-            val linesOfCells = lines
-                .map { line -> line.map(Char::toCell) }
-                .toList()
-            val size = Size(
-                rowCount = linesOfCells.size,
-                colCount = linesOfCells.first().size
-            )
-            val finalGrid = SeatingSystem(
-                MutableMapGrid(size) { row, col ->
-                    linesOfCells[row][col]
-                },
-                EightNeighborsGridCellCellStateChangeDeterminer()
-            ).finalState()
-            size
-                .allPositions()
-                .map(finalGrid::get)
-                .count(OCCUPIED_SEAT::equals)
-        }
+        occupiedSeatsCountAfterEquilibrium(
+            EightNearestNeighborsGridCellCellStateChangeDeterminer(4)
+        )
     )
+
+private fun partTwo() =
+    println(
+        occupiedSeatsCountAfterEquilibrium(
+            EightVisibleGridCellCellStateChangeDeterminer(5)
+        )
+    )
+
+private fun occupiedSeatsCountAfterEquilibrium(gridCellCellStateChangeDeterminer: GridCellStateChangeDeterminer<MutableMapGrid<Cell>>) =
+    Path.of("input", "day11", "input.txt").useLinesFromResource { lines ->
+        val grid = lines.toGrid()
+        val finalGrid = SeatingSystem(
+            grid,
+            gridCellCellStateChangeDeterminer
+        ).equilibriumState()
+        finalGrid.size
+            .allPositions()
+            .map(finalGrid::get)
+            .count(OccupiedSeat::equals)
+    }
+
+private fun Sequence<String>.toGrid(): MutableMapGrid<Cell> {
+    val linesOfCells = map { line -> line.map(Char::toCell) }.toList()
+    val size = Size(
+        rowCount = linesOfCells.size,
+        colCount = linesOfCells.first().size
+    )
+    return MutableMapGrid(size) { row, col ->
+        linesOfCells[row][col]
+    }
+}
 
 private class SeatingSystem<G : Grid<Cell, G>>(
     private var grid: G,
     private val gridCellStateChangeDeterminer: GridCellStateChangeDeterminer<G>,
 ) {
-    fun finalState(): G {
+    fun equilibriumState(): G {
         val stateChanges: MutableMap<Position, Cell> = mutableMapOf()
         do {
             stateChanges.clear()
@@ -68,32 +81,22 @@ private fun interface GridCellStateChangeDeterminer<G : Grid<Cell, G>> {
     fun determineAtPosition(pos: Position, grid: G): StateChange?
 }
 
-private class EightNeighborsGridCellCellStateChangeDeterminer<G : Grid<Cell, G>> : GridCellStateChangeDeterminer<G> {
-    companion object {
-        fun Position.eightNeighbors() = sequence {
-            yield(this@eightNeighbors.plusCol(1))
-            yield(this@eightNeighbors.plusCol(-1))
-            yield(this@eightNeighbors.plusRow(1))
-            yield(this@eightNeighbors.plusRow(-1))
-            yield(this@eightNeighbors.plus(1, 1))
-            yield(this@eightNeighbors.plus(-1, 1))
-            yield(this@eightNeighbors.plus(1, -1))
-            yield(this@eightNeighbors.plus(-1, -1))
-        }
-    }
-
+private class VisibleSeatsCheckGridCellStateChangeDeterminer<G : Grid<Cell, G>>(
+    private val occupiedSeatsThreshold: Int,
+    private val visibleSeatsFindingStrategy: (pos: Position, grid: G) -> Sequence<Seat>,
+) : GridCellStateChangeDeterminer<G> {
     override fun determineAtPosition(pos: Position, grid: G): StateChange? {
-        val eightNeighbors by lazy { pos.eightNeighbors().filter(grid.size::contains).map(grid::get) }
+        val visibleSeats by lazy { visibleSeatsFindingStrategy(pos, grid) }
         return when (grid[pos]) {
-            EMPTY_SEAT -> {
-                if (!eightNeighbors.any(OCCUPIED_SEAT::equals))
-                    StateChange(OCCUPIED_SEAT)
+            EmptySeat -> {
+                if (!visibleSeats.any(OccupiedSeat::equals))
+                    StateChange(OccupiedSeat)
                 else
                     null
             }
-            OCCUPIED_SEAT -> {
-                if (eightNeighbors.count(OCCUPIED_SEAT::equals) >= 4)
-                    StateChange(EMPTY_SEAT)
+            OccupiedSeat -> {
+                if (visibleSeats.count(OccupiedSeat::equals) >= occupiedSeatsThreshold)
+                    StateChange(EmptySeat)
                 else
                     null
             }
@@ -101,6 +104,75 @@ private class EightNeighborsGridCellCellStateChangeDeterminer<G : Grid<Cell, G>>
         }
     }
 }
+
+private class EightNearestNeighborsGridCellCellStateChangeDeterminer<G : Grid<Cell, G>> private constructor(
+    private val visibleSeatsCheckGridCellStateChangeDeterminer: VisibleSeatsCheckGridCellStateChangeDeterminer<G>,
+) : GridCellStateChangeDeterminer<G> by visibleSeatsCheckGridCellStateChangeDeterminer {
+    constructor(occupiedSeatsThreshold: Int) : this(
+        VisibleSeatsCheckGridCellStateChangeDeterminer(occupiedSeatsThreshold, ::visibleSeats)
+    )
+
+    companion object {
+        private fun <G : Grid<Cell, G>> visibleSeats(pos: Position, grid: G): Sequence<Seat> =
+            sequenceOf(
+                pos.plusCol(1),
+                pos.plusCol(-1),
+                pos.plusRow(1),
+                pos.plusRow(-1),
+                pos.plus(1, 1),
+                pos.plus(-1, 1),
+                pos.plus(1, -1),
+                pos.plus(-1, -1),
+            )
+                .filter(grid.size::contains)
+                .map(grid::get)
+                .filterIsInstance<Seat>()
+    }
+}
+
+private class EightVisibleGridCellCellStateChangeDeterminer<G : Grid<Cell, G>> private constructor(
+    private val visibleSeatsCheckGridCellStateChangeDeterminer: VisibleSeatsCheckGridCellStateChangeDeterminer<G>,
+) : GridCellStateChangeDeterminer<G> by visibleSeatsCheckGridCellStateChangeDeterminer {
+    constructor(occupiedSeatsThreshold: Int) : this(
+        VisibleSeatsCheckGridCellStateChangeDeterminer<G>(occupiedSeatsThreshold, ::visibleSeats)
+    )
+
+    companion object {
+        private fun <G : Grid<Cell, G>> visibleSeats(pos: Position, grid: G): Sequence<Seat> {
+            fun cellsInDirection(rowValue: Int, colValue: Int) = sequence {
+                var curPos = pos
+                while (true) {
+                    val nextPos = curPos.plus(rowValue, colValue)
+                    if (nextPos in grid.size) {
+                        yield(nextPos)
+                        curPos = nextPos
+                    } else {
+                        break
+                    }
+                }
+            }.map(grid::get)
+            return sequenceOf(
+                Direction(0, 1),
+                Direction(0, -1),
+                Direction(1, 0),
+                Direction(-1, 0),
+                Direction(1, 1),
+                Direction(-1, 1),
+                Direction(1, -1),
+                Direction(-1, -1),
+            )
+                .flatMap { (rowValue, colValue) ->
+                    sequenceOf(cellsInDirection(rowValue, colValue).filterIsInstance<Seat>().firstOrNull())
+                }
+                .filterNotNull()
+        }
+    }
+}
+
+private data class Direction(
+    val rowValue: Int,
+    val colValue: Int,
+)
 
 private fun Position.plus(rowValue: Int, colValue: Int): Position =
     copy(row = row + rowValue, col = col + colValue)
@@ -113,15 +185,15 @@ private fun Position.plusCol(colValue: Int): Position =
 
 private data class StateChange(val newState: Cell)
 
-private enum class Cell {
-    FLOOR,
-    EMPTY_SEAT,
-    OCCUPIED_SEAT,
-}
+private sealed class Cell
+private object Floor : Cell()
+private sealed class Seat : Cell()
+private object EmptySeat : Seat()
+private object OccupiedSeat : Seat()
 
 private fun Char.toCell(): Cell = when (this) {
-    '.' -> FLOOR
-    'L' -> EMPTY_SEAT
-    '#' -> OCCUPIED_SEAT
+    '.' -> Floor
+    'L' -> EmptySeat
+    '#' -> OccupiedSeat
     else -> throw IllegalArgumentException("Unknown cell $this")
 }
