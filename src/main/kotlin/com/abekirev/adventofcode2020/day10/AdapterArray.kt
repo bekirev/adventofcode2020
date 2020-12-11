337de93a1ff35eab5c4844525420cbadcff43b32
artechry <abekirev@gmail.com>
2020-12-11 17:57:15 +0300

    day 10 part 2 introduced more elegant solution

--- src/main/kotlin/com/abekirev/adventofcode2020/day10/AdapterArray.kt
+++ src/main/kotlin/com/abekirev/adventofcode2020/day10/AdapterArray.kt
@@ -1,7 +1,6 @@
 package com.abekirev.adventofcode2020.day10
 
 import com.abekirev.adventofcode2020.util.pair
-import com.abekirev.adventofcode2020.util.product
 import com.abekirev.adventofcode2020.util.useLinesFromResource
 import java.nio.file.Path
 
@@ -43,136 +42,17 @@ private fun partTwo() =
                 .map(String::toAdapter)
                 .sorted()
                 .toList()
-            val output = adapters.last() + DIFFERENCE_BETWEEN_LAST_AND_OUTPUT
-            val fullList = listOf(INPUT_RATE)
-                .plus(adapters)
-                .plus(output)
-            val validDifferenceRange = MIN_ADAPTER_DIFFERENCE..MAX_ADAPTER_DIFFERENCE
-            adapterArrayVariantsCount(
-                fullList,
-                validDifferenceRange,
+            val pathsCounts = mutableMapOf(
+                0 to 1L
             )
+            adapters.forEach { adapter ->
+                pathsCounts[adapter] = (MIN_ADAPTER_DIFFERENCE..MAX_ADAPTER_DIFFERENCE)
+                    .mapNotNull { difference -> pathsCounts[adapter - difference] }
+                    .sum()
+            }
+            pathsCounts[adapters.last()]
         }
     )
-
-private fun adapterArrayVariantsCount(
-    fullList: List<Adapter>,
-    validDifferenceRange: IntRange,
-): Long {
-    val indicesWithMaxDiffToTheNextElement = fullList.asSequence()
-        .mapIndexed { index, adapter ->
-            if (index != fullList.lastIndex && fullList[index + 1] - adapter == MAX_ADAPTER_DIFFERENCE) {
-                index
-            } else {
-                null
-            }
-        }
-        .filterNotNull()
-        .toList()
-    return formRanges(
-        indicesWithMaxDiffToTheNextElement,
-        fullList.size
-    )
-        .filter { it.second - it.first > 1 }
-        .map {
-            adapterArrayVariantsCount(
-                AdapterCollection(
-                    fullList.subList(
-                        it.first,
-                        it.second
-                    ),
-                    validDifferenceRange
-                )
-            )
-        }
-        .product()!!
-}
-
-private fun formRanges(
-    indicesForListEnds: List<Int>,
-    fullListSize: Int
-): Sequence<Pair<Int, Int>> = sequence {
-    var startIndex = 0
-    for (index in indicesForListEnds) {
-        yield(startIndex to index + 1)
-        startIndex = index + 1
-    }
-    if (startIndex != fullListSize - 1) {
-        yield(startIndex to fullListSize - 1)
-    }
-}
-
-private fun adapterArrayVariantsCount(
-    adapterCollection: AdapterCollection,
-): Long = adapterArrayVariantsCount(
-    adapterCollection,
-    adapterCollection.first(),
-)
-
-private fun adapterArrayVariantsCount(
-    adapterCollection: AdapterCollection,
-    lastAdapter: Adapter,
-): Long {
-    return when (lastAdapter) {
-        adapterCollection.last() -> 1
-        else -> {
-            val nextAdapters: Collection<Adapter> =
-                adapterCollection.adaptersCanConnectWithElementsAtIndex(adapterCollection.adapterIndex(lastAdapter)!!)
-            when {
-                nextAdapters.isEmpty() -> 0
-                else -> {
-                    nextAdapters
-                        .asSequence()
-                        .map { nextAdapter ->
-                            adapterArrayVariantsCount(
-                                adapterCollection,
-                                nextAdapter
-                            )
-                        }
-                        .sum()
-                }
-            }
-        }
-    }
-}
-
-private class AdapterCollection private constructor(
-    private val sortedAdapters: List<Adapter>,
-    private val validDifferenceRange: IntRange,
-) {
-    constructor(
-        adapters: Collection<Adapter>,
-        validDifferenceRange: IntRange,
-    ) : this(
-        adapters.sorted(),
-        validDifferenceRange,
-    )
-
-    private val adapterIndices: Map<Adapter, Int> by lazy {
-        sortedAdapters.mapIndexed { i, a -> a to i }.toMap()
-    }
-
-    fun adaptersCanConnectWithElementsAtIndex(index: Int): Collection<Adapter> {
-        val result = mutableSetOf<Adapter>()
-        val adapterAtIndex = sortedAdapters[index]
-        for (it in index + 1 until sortedAdapters.size) {
-            val itAdapter = sortedAdapters[it]
-            if (itAdapter - adapterAtIndex in validDifferenceRange) {
-                result.add(
-                    itAdapter
-                )
-            } else {
-                break
-            }
-        }
-        return result
-    }
-
-    fun adapterIndex(adapter: Adapter): Int? = adapterIndices[adapter]
-
-    fun first(): Adapter = sortedAdapters.first()
-    fun last(): Adapter = sortedAdapters.last()
-}
 
 private typealias Adapter = Int
 
