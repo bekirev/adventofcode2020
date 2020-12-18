d17f7a291e27399c3d58664831053f9db490e3aa
bekirev <abekirev@gmail.com>
2020-12-18 15:54:16 +0300

    day 17 part 2

--- src/main/kotlin/com/abekirev/adventofcode2020/day17/ConwayCubes.kt
+++ src/main/kotlin/com/abekirev/adventofcode2020/day17/ConwayCubes.kt
@@ -7,15 +7,30 @@ import java.nio.file.Path
 
 fun main() {
     partOne()
+    partTwo()
 }
 
-private fun partOne() {
+private fun partOne() =
+    println(
+        activeCubesAfterSixTicks { x, y ->
+            Position3D(x, y, z = 0)
+        }
+    )
+
+private fun partTwo() =
+    println(
+        activeCubesAfterSixTicks { x, y ->
+            Position4D(x, y, z = 0, w = 0)
+        }
+    )
+
+private fun <T : Neighborhood<T>> activeCubesAfterSixTicks(cubeFunc: (x: Int, y: Int) -> T): Int {
     var conwayCubesPocketDimension = Path.of("input", "day17", "input.txt").useLinesFromResource { lines ->
         ConwayCubesPocketDimension(
             lines
                 .flatMapIndexed { row, line ->
                     line.asSequence().mapIndexed { col, char ->
-                        Position3D(x = row, y = col, z = 0) to char.toCubeState()
+                        cubeFunc(col, row) to char.toCubeState()
                     }
                 }
                 .filter { it.second == ACTIVE }
@@ -25,28 +40,28 @@ private fun partOne() {
     repeat(6) {
         conwayCubesPocketDimension = conwayCubesPocketDimension.tick()
     }
-    println(conwayCubesPocketDimension.activeCubes().count())
+    return conwayCubesPocketDimension.activeCubes().count()
 }
 
-private class ConwayCubesPocketDimension private constructor(
-    private val activeCubes: MutableSet<Position3D>,
+private class ConwayCubesPocketDimension<T : Neighborhood<T>> private constructor(
+    private val activeCubes: MutableSet<T>,
 ) {
     constructor(
-        activeCubes: Sequence<Position3D>,
+        activeCubes: Sequence<T>,
     ) : this(
-        hashSetOf<Position3D>().apply {
+        hashSetOf<T>().apply {
             addAll(activeCubes)
         }
     )
 
-    private val inactiveCubes: Sequence<Position3D>
+    private val inactiveCubes: Sequence<T>
         get() =
             activeCubes
                 .asSequence()
-                .flatMap(Position3D::neighbors)
+                .flatMap { it.neighborhood() }
                 .filterNot(activeCubes::contains)
 
-    fun tick(): ConwayCubesPocketDimension {
+    fun tick(): ConwayCubesPocketDimension<T> {
         sequenceOf(
             inactiveCubes.map { position ->
                 position to if (position.activeNeighborsCount() == 3) ACTIVE else INACTIVE
@@ -65,38 +80,71 @@ private class ConwayCubesPocketDimension private constructor(
         return this
     }
 
-    private fun Position3D.activeNeighborsCount(): Int =
-        neighbors().count(activeCubes::contains)
+    private fun T.activeNeighborsCount(): Int =
+        neighborhood().count(activeCubes::contains)
 
-    fun activeCubes(): Sequence<Position3D> =
+    fun activeCubes(): Sequence<T> =
         activeCubes.asSequence()
+}
+
+private interface Neighborhood<T> {
+    fun neighborhood(): Sequence<T>
 }
 
 private data class Position3D(
     val x: Int,
     val y: Int,
     val z: Int,
-) {
+) : Neighborhood<Position3D> {
     companion object {
-        val ZERO = Position3D(0, 0, 0)
+        private val ZERO = Position3D(0, 0, 0)
     }
+
+    override fun neighborhood(): Sequence<Position3D> = sequence {
+        for (x in -1..1)
+            for (y in -1..1)
+                for (z in -1..1)
+                    yield(Position3D(x, y, z))
+    }
+        .filterNot(ZERO::equals)
+        .map { deltaPos -> this + deltaPos }
+
+    private operator fun Position3D.plus(other: Position3D): Position3D =
+        Position3D(
+            x + other.x,
+            y + other.y,
+            z + other.z,
+        )
 }
 
-private operator fun Position3D.plus(other: Position3D): Position3D =
-    Position3D(
-        x + other.x,
-        y + other.y,
-        z + other.z,
-    )
+private data class Position4D(
+    val x: Int,
+    val y: Int,
+    val z: Int,
+    val w: Int,
+) : Neighborhood<Position4D> {
+    companion object {
+        private val ZERO = Position4D(0, 0, 0, 0)
+    }
 
-private fun Position3D.neighbors(): Sequence<Position3D> = sequence {
-    for (x in -1..1)
-        for (y in -1..1)
-            for (z in -1..1)
-                yield(Position3D(x, y, z))
+    override fun neighborhood(): Sequence<Position4D> = sequence {
+        for (x in -1..1)
+            for (y in -1..1)
+                for (z in -1..1)
+                    for (w in -1..1)
+                        yield(Position4D(x, y, z, w))
+    }
+        .filterNot(ZERO::equals)
+        .map { deltaPos -> this + deltaPos }
+
+    private operator fun Position4D.plus(other: Position4D): Position4D =
+        Position4D(
+            x + other.x,
+            y + other.y,
+            z + other.z,
+            w + other.w,
+        )
 }
-    .filterNot(Position3D.ZERO::equals)
-    .map { deltaPos -> this + deltaPos }
 
 private enum class CubeState {
     ACTIVE,
