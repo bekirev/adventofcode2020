c4cb4239344a90b27037b6c2f0f61f3aacf564da
bekirev <abekirev@gmail.com>
2021-01-03 21:51:23 +0300

    day 22 part 2

--- src/main/kotlin/com/abekirev/adventofcode2020/day22/CrabCombat.kt
+++ src/main/kotlin/com/abekirev/adventofcode2020/day22/CrabCombat.kt
@@ -1,5 +1,7 @@
 package com.abekirev.adventofcode2020.day22
 
+import com.abekirev.adventofcode2020.day22.Winner.FIRST
+import com.abekirev.adventofcode2020.day22.Winner.SECOND
 import com.abekirev.adventofcode2020.util.useLinesFromResource
 import java.nio.file.Path
 import java.util.LinkedList
@@ -7,23 +9,38 @@ import java.util.Queue
 
 fun main() {
     partOne()
+    partTwo()
 }
 
 fun partOne() =
     println(
         CombatGame(
-            Path.of("input", "day22", "first_deck.txt").useLinesFromResource { it.deck() },
-            Path.of("input", "day22", "second_deck.txt").useLinesFromResource { it.deck() },
+            firstPlayerDeck(),
+            secondPlayerDeck(),
         ).winnerDeck().score()
     )
+
+fun partTwo() =
+    println(
+        RecursiveCombat(
+            firstPlayerDeck(),
+            secondPlayerDeck(),
+        ).winnerDeck().score()
+    )
+
+private fun secondPlayerDeck() = Path.of("input", "day22", "second_deck.txt").useLinesFromResource { it.deck() }
+
+private fun firstPlayerDeck() = Path.of("input", "day22", "first_deck.txt").useLinesFromResource { it.deck() }
 
 typealias SpaceCard = Int
 
 interface Deck {
     val isEmpty: Boolean
+    val size: Int
     fun drawTopCard(): SpaceCard?
     fun add(cards: List<SpaceCard>)
     val cards: List<SpaceCard>
+    fun copy(depth: Int): Deck
 }
 
 class QueueDeck private constructor(
@@ -38,6 +55,8 @@ class QueueDeck private constructor(
     override val isEmpty: Boolean
         get() = queue.isEmpty()
 
+    override val size: Int by queue::size
+
     override fun drawTopCard(): SpaceCard? =
         queue.poll()
 
@@ -47,16 +66,54 @@ class QueueDeck private constructor(
 
     override val cards: List<SpaceCard>
         get() = queue.toList()
+
+    override fun copy(depth: Int): Deck =
+        QueueDeck(queue.asSequence().take(depth))
+}
+
+interface Game {
+    val firstPlayerDeck: Deck
+    val secondPlayerDeck: Deck
+    fun round()
+}
+
+
+enum class Winner {
+    FIRST,
+    SECOND,
+    ;
+}
+
+fun Game.winner(): Winner {
+    tailrec fun Game.winner(winner: Winner?): Winner =
+        when (winner) {
+            is Winner -> winner
+            else -> {
+                when {
+                    firstPlayerDeck.isEmpty -> winner(SECOND)
+                    secondPlayerDeck.isEmpty -> winner(FIRST)
+                    else -> {
+                        round()
+                        winner(null)
+                    }
+                }
+            }
+        }
+    return winner(null)
+}
+
+fun Game.winnerDeck(): Deck = when(winner()) {
+    FIRST -> firstPlayerDeck
+    SECOND -> secondPlayerDeck
 }
 
 class CombatGame(
-    val firstPlayerDeck: Deck,
-    val secondPlayerDeck: Deck,
-) {
-    fun round() {
+    override val firstPlayerDeck: Deck,
+    override val secondPlayerDeck: Deck,
+) : Game {
+    override fun round() {
         val firstPlayerCard = firstPlayerDeck.drawTopCard() ?: throw IllegalStateException("First player has no cards")
-        val secondPlayerCard =
-            secondPlayerDeck.drawTopCard() ?: throw IllegalStateException("Second player has no cards")
+        val secondPlayerCard = secondPlayerDeck.drawTopCard() ?: throw IllegalStateException("Second player has no cards")
         when {
             firstPlayerCard > secondPlayerCard -> firstPlayerDeck.add(listOf(firstPlayerCard, secondPlayerCard))
             secondPlayerCard > firstPlayerCard -> secondPlayerDeck.add(listOf(secondPlayerCard, firstPlayerCard))
@@ -65,22 +122,51 @@ class CombatGame(
     }
 }
 
-fun CombatGame.winnerDeck(): Deck {
-    tailrec fun CombatGame.winnerDeck(winnerDeck: Deck?): Deck =
-        when (winnerDeck) {
-            is Deck -> winnerDeck
-            else -> {
+class DeckRepository private constructor(
+    private val set: MutableSet<Int>,
+) {
+    constructor() : this(hashSetOf())
+
+    fun add(deck: Deck) {
+        set.add(deck.cards.hashCode())
+    }
+
+    operator fun contains(deck: Deck): Boolean =
+        deck.cards.hashCode() in set
+}
+
+class RecursiveCombat(
+    override val firstPlayerDeck: Deck,
+    override val secondPlayerDeck: Deck,
+) : Game {
+    private val firstDeckRepository = DeckRepository()
+    private val secondDeckRepository = DeckRepository()
+
+    override fun round() {
+        if (firstPlayerDeck in firstDeckRepository || secondPlayerDeck in secondDeckRepository) {
+            firstPlayerDeck.add(listOf(
+                firstPlayerDeck.drawTopCard() ?: throw IllegalStateException("First player has no cards"),
+                secondPlayerDeck.drawTopCard() ?: throw IllegalStateException("Second player has no cards"),
+            ))
+        } else {
+            val firstPlayerCard = firstPlayerDeck.drawTopCard() ?: throw IllegalStateException("First player has no cards")
+            val secondPlayerCard = secondPlayerDeck.drawTopCard() ?: throw IllegalStateException("Second player has no cards")
+            if (firstPlayerDeck.size >= firstPlayerCard && secondPlayerDeck.size >= secondPlayerCard) {
+                when (RecursiveCombat(firstPlayerDeck.copy(firstPlayerCard), secondPlayerDeck.copy(secondPlayerCard)).winner()) {
+                    FIRST -> firstPlayerDeck.add(listOf(firstPlayerCard, secondPlayerCard))
+                    SECOND -> secondPlayerDeck.add(listOf(secondPlayerCard, firstPlayerCard))
+                }
+            } else {
                 when {
-                    firstPlayerDeck.isEmpty -> winnerDeck(secondPlayerDeck)
-                    secondPlayerDeck.isEmpty -> winnerDeck(firstPlayerDeck)
-                    else -> {
-                        round()
-                        winnerDeck(null)
-                    }
+                    firstPlayerCard > secondPlayerCard -> firstPlayerDeck.add(listOf(firstPlayerCard, secondPlayerCard))
+                    secondPlayerCard > firstPlayerCard -> secondPlayerDeck.add(listOf(secondPlayerCard, firstPlayerCard))
+                    else -> throw IllegalStateException("Two cards are the same")
                 }
             }
         }
-    return winnerDeck(null)
+        firstDeckRepository.add(firstPlayerDeck)
+        secondDeckRepository.add(secondPlayerDeck)
+    }
 }
 
 fun Deck.score(): Int {
