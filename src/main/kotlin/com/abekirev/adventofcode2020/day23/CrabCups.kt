a417efce76dd98b3c806fd120966c70f4a73a61f
artechry <abekirev@gmail.com>
2021-01-04 18:12:33 +0300

    day 23 part 2

--- src/main/kotlin/com/abekirev/adventofcode2020/day23/CrabCups.kt
+++ src/main/kotlin/com/abekirev/adventofcode2020/day23/CrabCups.kt
@@ -2,70 +2,118 @@ package com.abekirev.adventofcode2020.day23
 
 fun main() {
     partOne()
+    partTwo()
 }
 
-private fun partOne() =
+private val input = "389547612".map { it.toString().toInt() }
+
+private fun partOne() {
+    val (circledLinkedList, head) = move(input, 100)
     println(
-        "389547612".map { it.toString().toInt() }
-            .labelsAfterOne(100)
-            .joinToString("")
+        circledLinkedList.removeAfter(1, input.size - 1).joinToString("")
     )
+}
+
+private fun partTwo() {
+    val (circledLinkedList, head) = move(
+        sequenceOf(input.asSequence(), generateSequence(input.maxOrNull()!! + 1, 1::plus))
+        .flatten()
+        .take(1_000_000)
+        .toList(),
+        10_000_000
+    )
+    println(circledLinkedList.removeAfter(1, 2).reduce(Int::times))
+}
+
+fun move(items: List<Cup>, movesCount: Int): Pair<CircledLinkedList<Cup>, Cup> {
+    val circledLinkedList = CircledLinkedList(items)
+    val min = items.minOrNull()!!
+    val max = items.maxOrNull()!!
+    fun destinationSequence(head: Cup): Sequence<Cup> = sequence {
+        var cup = head
+        while (true) {
+            cup = when (cup) {
+                min -> max
+                else -> cup - 1
+            }
+            yield(cup)
+        }
+    }
+    var head = input.first()
+    repeat(movesCount) {
+        val pickup = circledLinkedList.removeAfter(head, 3)
+        val destination = destinationSequence(head).first(circledLinkedList::contains)
+        circledLinkedList.placeAfter(destination, pickup)
+        head = circledLinkedList.elemAfter(head)
+    }
+    return circledLinkedList to head
+}
 
 typealias Cup = Int
 
-fun List<Cup>.labelsAfterOne(movesCount: Int): List<Cup> =
-    after(movesCount).reorderCircledWithStartElement(1).drop(1)
+class CircledLinkedList<T>(items: List<T>) {
+    private val elements: MutableMap<T, Node<T>>
 
-fun <T> List<T>.reorderCircledWithStartElement(elem: T): List<T> = when (val elemIndex = indexOf(elem)) {
-    -1 -> throw IllegalArgumentException("Element $elem is not in list $this")
-    else -> reorderCircledWithStartIndex(elemIndex)
-}
-
-fun <T> List<T>.reorderCircledWithStartIndex(index: Int): List<T> = when (index) {
-    0 -> this
-    else -> subList(index, size) + subList(0, index)
-}
-
-fun List<Cup>.after(movesCount: Int): List<Cup> {
-    var cups = this
-    repeat(movesCount) {
-        cups = move(cups)
-    }
-    return cups
-}
-
-fun move(cups: List<Cup>): List<Cup> {
-    val pickup = cups.subList(1, 4)
-    val first = cups.first()
-    return when (val destinationCup = cups.destinationCup(pickup)) {
-        first -> cups
-        else -> sequenceOf(
-            sequenceOf(first),
-            cups.subList(4, cups.size).asSequence()
-        ).flatten().placeAfter(destinationCup, pickup).toList()
-    }.reorderCircledWithStartIndex(1)
-}
-
-fun <T> Sequence<T>.placeAfter(elem: T, list: List<T>): Sequence<T> = sequence {
-    for (it in this@placeAfter) {
-        when (it) {
-            elem -> {
-                yield(elem)
-                yieldAll(list)
+    init {
+        val elements = HashMap<T, Node<T>>()
+        var first: Node<T>? = null
+        var prev: Node<T>? = null
+        for (item in items) {
+            if (first == null) {
+                first = Node(item)
+                prev = first
+            } else {
+                prev = prev?.let { p ->
+                    p.next = Node(item)
+                    p.next
+                }
             }
-            else -> yield(it)
+            prev?.let { p ->
+                elements[p.value] = p
+            }
+        }
+        if (first != null) {
+            prev!!.next = first
+        }
+        this.elements = elements
+    }
+
+    private data class Node<T>(
+        val value: T,
+        var next: Node<T>? = null,
+    )
+
+    fun removeAfter(elem: T, count: Int): List<T> {
+        return when (count) {
+            0 -> emptyList()
+            else -> {
+                val node = elements[elem] ?: throw IllegalArgumentException("Element $elem is not present")
+                val list = mutableListOf<T>()
+                var i = 0
+                while (i < count) {
+                    val nextNode = node.next!!
+                    list.add(nextNode.value)
+                    elements.remove(nextNode.value)
+                    node.next = nextNode.next
+                    ++i
+                }
+                return list
+            }
         }
     }
-}
 
-fun List<Cup>.destinationCup(pickup: List<Cup>): Cup {
-    val min = minOrNull()!!
-    val max = maxOrNull()!!
-    tailrec fun destinationCup(destinationCup: Cup): Cup =
-        when {
-            destinationCup < min -> destinationCup(max)
-            destinationCup in pickup -> destinationCup(destinationCup - 1)
-            else -> destinationCup
+    fun placeAfter(elem: T, list: List<T>) {
+        var node = elements[elem] ?: throw IllegalArgumentException("Element $elem is not present")
+        for (item in list) {
+            val nextNode = Node(item, node.next)
+            elements[item] = nextNode
+            node.next = nextNode
+            node = nextNode
         }
-    return destinationCup(first() - 1)
+    }
+
+    fun elemAfter(elem: T): T =
+        (elements[elem] ?: throw IllegalArgumentException("Element $elem is not present")).next!!.value
+
+    operator fun contains(elem: T): Boolean = elements.containsKey(elem)
 }
--- src/test/kotlin/com/abekirev/adventofcode2020/day23/CrabCupsTest.kt
+++ src/test/kotlin/com/abekirev/adventofcode2020/day23/CrabCupsTest.kt
@@ -1,74 +1,13 @@
 package com.abekirev.adventofcode2020.day23
 
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.data.blocking.forAll
-import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class CrabCupsTest : ShouldSpec({
-    should("return labels after 1 element after given number of moves") {
-        forAll(
-            row(listOf(3, 8, 9, 1, 2, 5, 4, 6, 7), 10, listOf(9, 2, 6, 5, 8, 3, 7, 4)),
-            row(listOf(3, 8, 9, 1, 2, 5, 4, 6, 7), 100, listOf(6, 7, 3, 8, 4, 5, 2, 9)),
-        ) { input, movesCount, result ->
-            input.labelsAfterOne(movesCount) shouldBe result
-        }
-    }
     should("perform moves") {
-        listOf(3, 8, 9, 1, 2, 5, 4, 6, 7).after(10) shouldBe listOf(8, 3, 7, 4, 1, 9, 2, 6, 5)
-    }
-    should("should perform move") {
-        forAll(
-            row(
-                listOf(3, 8, 9, 1, 2, 5, 4, 6, 7),
-                listOf(2, 8, 9, 1, 5, 4, 6, 7, 3),
-            ),
-            row(
-                listOf(2, 8, 9, 1, 5, 4, 6, 7, 3),
-                listOf(5, 4, 6, 7, 8, 9, 1, 3, 2),
-            ),
-            row(
-                listOf(5, 4, 6, 7, 8, 9, 1, 3, 2),
-                listOf(8, 9, 1, 3, 4, 6, 7, 2, 5),
-            ),
-        ) { input, result ->
-            move(input) shouldBe result
-        }
-    }
-    should("reorder circled with start element") {
-        forAll(
-            row(listOf(1), 1, listOf(1)),
-            row(listOf(1, 2), 1, listOf(1, 2)),
-            row(listOf(4, 2, 3), 2, listOf(2, 3, 4)),
-            row(listOf(8, 5, 6), 6, listOf(6, 8, 5)),
-            row(listOf(10, 4, 6, 4), 4, listOf(4, 6, 4, 10)),
-        ) { input, elem, result ->
-            input.reorderCircledWithStartElement(elem) shouldBe result
-        }
-    }
-    should("determine destination cup") {
-        forAll(
-            row(listOf(6, 4, 3, 7, 2, 5), listOf(4, 3), 5),
-            row(listOf(4, 3, 6, 7, 2, 5), listOf(3, 6), 2),
-            row(listOf(3, 1, 2, 4, 6, 5), listOf(1, 2), 6),
-        ) { cups, pickup, result ->
-            cups.destinationCup(pickup) shouldBe result
-        }
-    }
-    should("place list of elements after elem") {
-        forAll(
-            row(sequenceOf(4), 3, listOf(1), listOf(4)),
-            row(sequenceOf(5), 5, listOf(), listOf(5)),
-            row(sequenceOf(3, 2, 3), 3, listOf(1, 2), listOf(3, 1, 2, 2, 3, 1, 2)),
-        ) { input, elem, list, result ->
-            input.placeAfter(elem, list).toList() shouldBe result
-        }
-    }
-    should("reorder circled with start index") {
-        forAll(
-            row(listOf(1, 6, 3), 0, listOf(1, 6, 3)),
-        ) { input, index, result ->
-            input.reorderCircledWithStartIndex(index) shouldBe result
-        }
+        val resPair = move(listOf(3, 8, 9, 1, 2, 5, 4, 6, 7), 10)
+        val expectedResult = listOf(8, 3, 7, 4, 1, 9, 2, 6, 5)
+        resPair.second shouldBe expectedResult.first()
+        resPair.first.removeAfter(resPair.second, expectedResult.size - 1) shouldBe expectedResult.subList(1, expectedResult.size)
     }
 })
